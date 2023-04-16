ecf6cb6a4c45fa2a21b47e98e690854b391f23a5
eliasgauna <eliaslorenzogauna@gmail.com>
2023-04-16 18:25:05 -0300

    16deAbril

--- trabajo integrador.js
+++ trabajo integrador.js
@@ -1,30 +1,61 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 3000,
-  wrap: false
-})
 
-function mostrar(card) {
-    document.getElementById(boton).style.display = 'block';
-    document.getElementById(card).style.display = 'none';
-    btn.style.display = 'none';
-}
-
-
-document.getElementById("reseña").addEventListener("submit", function(e) {
-  e.preventDefault(); // Evita que el formulario se envíe
-
-  // Obtén los valores del formulario
+/*JS del resumen*/
+ 
+function mostrarResumen(){
   var nombrereseña = document.getElementById("nombrer").value;
   var textoreseña = document.getElementById("reseñar").value;
 
-  // Crea un nuevo objeto jsPDF
-  var pdf = new jsPDF();
+  var nombreResumen = document.getElementById("nombre-resumen");
+  var resenaResumen = document.getElementById("resena-resumen");
 
-  // Agrega el contenido al PDF
-  pdf.text(20, 20, "Nombre del cliente: " + nombrereseña);
-  pdf.text(20, 50, "Reseña del cliente: " + textoreseña);
+  nombreResumen.innerHTML = nombrereseña;
+  resenaResumen.innerHTML = textoreseña;
 
-  // Guarda el PDF como un archivo
-  pdf.save("reseña de clientes.pdf");
-});
+  var sectionResena = document.getElementById("resena");
+  var sectionResumen = document.getElementById("resumen");
+
+  sectionResena.classList.remove("visible");
+  sectionResena.classList.add("oculta");
+
+  sectionResumen.classList.remove("oculta");
+  sectionResumen.classList.add("visible");
+
+}
+
+
+
+/*JS exportación a PDF*/
+
+function exportarPDF (){
+  var element = document.getElementById('resumen');
+  var opt = {
+  margin:       [50,50],
+  filename:     'resena.pdf',
+  jsPDF:        { unit: 'mm', format: 'a4'}
+};
+
+html2pdf().set(opt).from(element).save();
+}
+
+
+
+/*JS consumo de la API externa*/
+
+async function representanteUno(){
+  const representante = await fetch("https://reqres.in/api/users/2"); //Consumo
+  const datoJSON = await representante.json(); //Paso los datos a json
+
+  
+  var nombreRep = document.getElementById('nombre-representante');
+  var emailRep = document.getElementById('email-representante');
+
+  nombreRep.innerHTML = "Nombre: " + datoJSON.data.first_name + " " + datoJSON.data.last_name;
+  emailRep.innerHTML = "Email: " + datoJSON.data.email;
+
+  const avatar = document.createElement("img") //Creo el elemento imagen
+  avatar.setAttribute("src", `${datoJSON.data.avatar}`)
+  avatar.setAttribute("alt", "una imagen");
+
+  var sectionAvatar = document.getElementById("representante-avatar");
+  sectionAvatar.appendChild(avatar); //Agrego la imagen al div
+}
